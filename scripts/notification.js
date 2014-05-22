3e7bdba01ab7b0ffee2f4afee9ea65f47dfc7f23
Simon Zheng <simonzheng@stanford.edu>
2014-05-22 15:38:28 -0700

    notification stuff

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,3 +1,9 @@
+var otherPersonsName = 'Nusi';
+var otherPersonsGender = 'her';
+var otherPersonsFbUrl = 'ncvue';
+var timeFrame = '1 year';
+var eventThatWeNotifyThemAbout = 'you went to formal with';
+
 // Make the "!" blink over the chrome icon to indicate notification
 var ANIMATION_TIME_IN_MS = 500;
 var counter = 0;
@@ -26,20 +32,21 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.greeting=="sendnotificationbitch") {
       var options = {
-      type: "basic",
-      title: "It's been 1 year since you sucked ranajays dick",
-      message: "Message him on Facebook?",
-      iconUrl: "images/sparkers_icon_128.png"
-    }
+        type: "basic",
+        title: "It's been exactly " + timeFrame + " since " + eventThatWeNotifyThemAbout + " " + otherPersonsName + "!",
+        message: "Message " + otherPersonsGender + " on Facebook?",
+        iconUrl: "images/sparkers_icon_128.png"
+      };
     chrome.notifications.create("", options, function() {});
     console.log("nOTIFICATION BITCHEZ");
-  }
+    startNotificationAnimation();
+    }
   });
 
 chrome.notifications.onClicked.addListener(function(notificationID) {
-  window.open("http://www.facebook.com/messages/ranajays");
-  startNotificationAnimation();
+  window.open("http://www.facebook.com/messages/" + otherPersonsFbUrl);
   setTimeout(function() {
-    alert("Click the Orange Sparkers Icon in the top right to view mutual content to share!");
+    alert("Click the Orange Sparkers Icon in the top right to view mutual content between you and " + otherPersonsName + " to share!");
   }, 2000);
-})
+  stopNotificationAnimation();
+});
