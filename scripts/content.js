5d56bf9bececa07bb86350659cf84d124de70e26
Simon Zheng <simonzheng@stanford.edu>
2014-06-03 01:01:25 -0700

    finished landing page but hardcoded values. @ranajays, you might need to tweak some css in content.js to make it work on your computer if we have multiple demos

--- scripts/content.js
+++ scripts/content.js
@@ -48,31 +48,80 @@ function showGuidePage() {
   // create overlay and append to page
   var overlay = document.createElement("div");
   overlay.setAttribute("id","overlay");
+  overlay.setAttribute('style', 'background-color: #000;opacity: .7;filter: alpha(opacity=70);position: fixed; top: 0; left: 0;width: 100%; height: 100%;z-index: 2147483646;text-align: center;color: white;font-size: 36px;');
   document.body.appendChild(overlay);
 
-  var firstGuideMessageDiv = document.createElement("div");
-  firstGuideMessageDiv.setAttribute('style', 'width:60%;');
-  overlay.appendChild(firstGuideMessageDiv);
+  var guideMessageDiv = document.createElement("div");
+  guideMessageDiv.setAttribute('style', 'width:60%;');
+  overlay.appendChild(guideMessageDiv);
 
   // Overlay Paragraph
   var overlayParagraph1 = document.createElement("p");
-  overlayParagraph1.innerHTML = 'Click the orange <strong>Sparkers</strong> icon';
-  firstGuideMessageDiv.appendChild(overlayParagraph1);
-
-  // Sparkers icon image
-  var iconImage = document.createElement("img");
-  iconImage.src = 'http://i.imgur.com/r2GDh5e.png';
-  firstGuideMessageDiv.appendChild(iconImage);
+  overlayParagraph1.innerHTML = 'Click the orange <strong>Sparkers</strong> icon<br/><br/><br/><br/>';
+  guideMessageDiv.appendChild(overlayParagraph1);
 
   // Overlay paragraph 2
   var overlayParagraph2 = document.createElement("p");
   overlayParagraph2.innerHTML = 'in the <strong>top right</strong> to view and copy mutual content between you two!';
-  firstGuideMessageDiv.appendChild(overlayParagraph2);
+  guideMessageDiv.appendChild(overlayParagraph2);
 
-  // Add styles
-  overlay.setAttribute('style', 'background-color: #000;opacity: .7;filter: alpha(opacity=70);position: fixed; top: 0; left: 0;width: 100%; height: 100%;z-index: 2147483646;text-align: center;color: white;font-size: 36px;');
-  // img.setAttribute('style', ' position: absolute;z-index: 2147483647;left: 40%;top: 40%;margin: 0 auto;');
+  // Sparkers icon image
+  var iconImage = document.createElement("img");
+  iconImage.src = 'http://i.imgur.com/r2GDh5e.png';
+  iconImage.setAttribute("id","iconImage");
+  iconImage.setAttribute('style', 'position: fixed; top: 100px; left: 300px;z-index: 2147483647;');
+  // iconImage.setAttribute('style', 'z-index: 2147483647; opacity: 1.0; filter: alpha(opacity=100); margin: 0 auto;');
+  document.body.appendChild(iconImage);
+
+  // Add arrow1
+  var arrow1 = document.createElement("img");
+  arrow1.setAttribute("id","arrow1");
+  arrow1.setAttribute('style', 'position: fixed; top: 0px; right: 100px;z-index: 2147483647;');
+  arrow1.src = "http://i.imgur.com/ZodwLNm.png";
+  document.body.appendChild(arrow1);
+
+  // Add arrow2
+  var arrow2 = document.createElement("img");
+  arrow2.setAttribute("id","arrow2");
+  arrow2.setAttribute('style', 'position: fixed; top: 0px; right: 70px;z-index: 2147483647;');
+  arrow2.src = "http://i.imgur.com/IxnI9O0.png";
+  document.body.appendChild(arrow2);
+
+  // Paragraph after arrow 2
+  var guideMessageDiv2 = document.createElement("div");
+  guideMessageDiv2.setAttribute('id', 'guideMessageDiv2');
+  guideMessageDiv2.setAttribute('style', 'width:15%; position: fixed; right: 0px; top:170px; font-size: 26px;');
+  overlay.appendChild(guideMessageDiv2);
+  var paraAfterArrow2 = document.createElement("p");
+  paraAfterArrow2.innerHTML = 'Sparkers shows your mutual content';
+  guideMessageDiv2.appendChild(paraAfterArrow2);
+
+  // Add sparkers panel
+  var sparkersPanel = document.createElement("img");
+  sparkersPanel.setAttribute("id","sparkersPanel");
+  sparkersPanel.setAttribute('style', 'position: fixed; top: 300px; right: 0px;z-index: 2147483647;');
+  sparkersPanel.src = "http://i.imgur.com/fdm4v7b.png";
+  document.body.appendChild(sparkersPanel);
+
+  // Add arrow3
+  var arrow3 = document.createElement("img");
+  arrow3.setAttribute("id","arrow3");
+  arrow3.setAttribute('style', 'position: fixed; top: 450px; right: 200px;z-index: 2147483647;');
+  arrow3.src = "http://i.imgur.com/LFnv91T.png";
+  document.body.appendChild(arrow3);
+
+  // Paragraph after arrow 3
+  var guideMessageDiv3 = document.createElement("div");
+  guideMessageDiv3.setAttribute('id', 'guideMessageDiv3');
+  guideMessageDiv3.setAttribute('style', 'width:20%; position: fixed; right: 200px; top:300px; font-size: 26px;');
+  overlay.appendChild(guideMessageDiv3);
+  var paraAfterArrow3 = document.createElement("p");
+  paraAfterArrow3.innerHTML = 'Click to copy a link to old content and send it in Facebook Messenger';
+  guideMessageDiv3.appendChild(paraAfterArrow3);
   
+  
+  
+
   // click to restore page
   $(document).click(function() {
     restore();
@@ -83,6 +132,13 @@ function showGuidePage() {
 // restore page to normal
 function restore() {
   document.body.removeChild(document.getElementById("overlay"));
+  document.body.removeChild(document.getElementById("iconImage"));
+  document.body.removeChild(document.getElementById("arrow1"));
+  document.body.removeChild(document.getElementById("arrow2"));
+  document.body.removeChild(document.getElementById("arrow3"));
+  document.body.removeChild(document.getElementById("sparkersPanel"));
+  document.body.removeChild(document.getElementById("guideMessageDiv2"));
+  
 }
 // ********************************************************
 // ********************************************************
@@ -91,6 +147,7 @@ function restore() {
 
 window.onload = function(){ // this could be done faster with the livequery() plugin for jquery
     var facebookMsgsURL = "https://www.facebook.com/messages/";
+    console.log("document.URL is " + document.URL);
     if (document.URL.length >= facebookMsgsURL.length && document.URL.substring(0, facebookMsgsURL.length) == facebookMsgsURL) {
       showGuidePage(); // Show the guide page on load
       console.log("adding shit.");
