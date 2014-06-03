5c2c0f0e9c170dfe36d9169da72a01654e9e9bc1
Lawrence Xiong Rogers <lawrence.x.rogers@gmail.com>
2014-06-03 15:12:13 -0700

    added 100 character text limit to descriptions

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,6 +4,11 @@ var sparksTemplate = $('#sparks-template');
 var templates = {
         renderSparksImages: Handlebars.compile(sparksTemplate.html())
 };
+Handlebars.registerHelper('limit', function(str) {
+  if (str.length > 100)
+    return str.substring(0,100) + '...';
+  return str;
+});
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {greeting: "getData"}, function(response) {
